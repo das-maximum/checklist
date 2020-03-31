5d786e0b8eac0e8156da747bbda2e3490cb4c0d5
Maximilian Krickl <krickl@quinesoft.de>
2020-03-31 21:21:19 +0200

    Correcting JSON Format

--- src/main/scala/de/quinesoft/checklist/model/ToDoItemJsonProtocol.scala
+++ src/main/scala/de/quinesoft/checklist/model/ToDoItemJsonProtocol.scala
@@ -1,12 +1,31 @@
 package de.quinesoft.checklist.model
 
-import spray.json.DefaultJsonProtocol
-import spray.json._
+import java.time.Instant
+import java.time.format.DateTimeParseException
+
+import spray.json.{DefaultJsonProtocol, DeserializationException, JsString, JsValue, RootJsonFormat}
 
 /**
  * @author <a href="mailto:krickl@quinesoft.de>Maximilian Krickl</a>
  */
-class ToDoItemJsonProtocol extends DefaultJsoProtocol {
+class ToDoItemJsonProtocol extends DefaultJsonProtocol {
 
-  val toDoItemJsonProtocol = rootFormat(jsonFormat4(ToDoItem.apply))
+  implicit val instantProtocol: RootJsonFormat[Instant] = new RootJsonFormat[Instant] {
+    override def read(json: JsValue): Instant = json match {
+      case JsString(value) =>
+        try {
+          Instant.parse(value)
+        } catch {
+          case _: DateTimeParseException =>
+            throw DeserializationException(s"Timestamp has wrong format. Must be like this: 2020-03-31T21:10:43Z. Found $s")
+        }
+
+      case _ =>
+        throw DeserializationException(s"Timestamp is no string. Found ${json.prettyPrint}")
+    }
+
+    override def write(obj: Instant): JsValue = JsString(obj.toString)
+  }
+
+  implicit val toDoItemJsonProtocol: RootJsonFormat[ToDoItem] = rootFormat(jsonFormat4(ToDoItem.apply))
 }
