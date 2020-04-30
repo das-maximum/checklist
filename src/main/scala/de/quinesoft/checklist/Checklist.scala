1c67d4d6a22b91aa790b4ebbc686e53dc98b768d
Maximilian Krickl <krickl@quinesoft.de>
2020-04-30 22:35:58 +0200

    Adapt to new config

--- src/main/scala/de/quinesoft/checklist/Checklist.scala
+++ src/main/scala/de/quinesoft/checklist/Checklist.scala
@@ -6,17 +6,19 @@ import com.typesafe.scalalogging.Logger
 import de.quinesoft.checklist.config.ChecklistConfig
 import de.quinesoft.checklist.routes.Routing
 import pureconfig.ConfigSource
+import scala.concurrent.ExecutionContext
 
 /**
  * @author <a href="mailto:krickl@quinesoft.de>Maximilian Krickl</a>
  */
 object Checklist extends App {
   private val logger: Logger = Logger(Checklist.getClass.getCanonicalName)
+  private implicit val ec: ExecutionContext = ExecutionContext.Implicits.global
   private implicit val system: ActorSystem = ActorSystem("checklist")
 
   logger.info("Loading config")
   val config: ChecklistConfig = ConfigSource.default.loadOrThrow[ChecklistConfig]
 
   logger.info("Starting server")
-  Http().bindAndHandle(Routing.routes, config.host, config.port)
+  Http().bindAndHandle(new Routing(config).routes, config.host, config.port.number)
 }
